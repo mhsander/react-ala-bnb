75fcde0bd4b800f394638807ec32b6372c3bb679
mhsander <marthahsander@gmail.com>
2018-07-05 11:46:15 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import Greeter from './greeter';
+
+function App() {
+  return (
+    <div>
+      <h1>Oi!</h1>
+      <p>hey!</p>
+    </div>
+  );
+}
+
+export default App;
