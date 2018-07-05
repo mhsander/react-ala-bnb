bacd50715436132dc39ccde4e03d2b82e0e6b589
mhsander <marthahsander@gmail.com>
2018-07-05 12:03:51 -0700

    use class component for App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-//import Greeter from './greeter';
+import Greeter from './Greeter';
 
-function App() {
-  return (
-    <div>
-      <h1>Oi!</h1>
-      <p>hey!</p>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <h1>Oi!</h1>
+//       <p>hey!</p>
+//       <Greeter />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  componentDidMount() {
+    console.log('run only once');
+  }
+
+  componentDidUpdate() {
+    console.log('run on update');
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Oi!</h1>
+        <p>hey!</p>
+        <Greeter />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Greeter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function Greeter() {
+  return (
+    <div>
+      <section>
+        <h2>Greeter!</h2>
+      </section>
+    </div>
+  );
+}
+
+
